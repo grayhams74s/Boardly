5b5a26131fc699b87a47678971d3e0357b37670d
grayhams74s <felixjunang@gmail.com>
2026-07-09 13:03:00 +0800

    ListView

---
+++ client/src/app/projects/ListView/index.tsx
@@ -0,0 +1,30 @@
+import { useGetTasksQuery } from '@/state/api';
+import React from 'react'
+
+type Props = {
+    id: string;
+    setIsModalNewTaskOpen: (isOpen: boolean) =>  void;
+}
+
+
+const ListView =  ({ id, setIsModalNewTaskOpen }: Props) => {
+
+    const {
+        data: tasks,
+        error,
+        isLoading,
+      } = useGetTasksQuery({ projectId: Number(id) });      
+
+      if (isLoading) return <div>Loading...</div>;
+      if (error) return <div> An Error  occured while fetching tasks</div>
+
+  return (
+    <div className='px-4 pb-8 xl:px-6'>
+        <div  className='pt-5'> 
+
+        </div>
+    </div>
+  )
+}
+
+export default ListView
