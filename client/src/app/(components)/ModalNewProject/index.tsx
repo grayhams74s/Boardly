3eb03e9e64b196add3096cc8bb58ab7098553fa5
grayhams74s <felixjunang@gmail.com>
2026-07-11 17:35:55 +0800

    modal update

--- client/src/app/(components)/ModalNewProject/index.tsx
+++ client/src/app/(components)/ModalNewProject/index.tsx
@@ -1,5 +1,5 @@
 import { useCreateProjectMutation } from '@/state/api';
-import React from 'react'
+import React, { useState } from 'react'
 
 
 type Props = {
@@ -9,6 +9,31 @@ type Props = {
 
 const ModalNewProject = ({ isOpen, onClose }: Props) => {
     const [createProject, { isLoading }] = useCreateProjectMutation();
+    const [projectName, setProjectName] = useState("");
+    const [description, setDescription] = useState("")
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
+
+    const handleSubmit = async () => {
+        if (!projectName || !startDate  || !endDate ) return
+
+        [/** API CALL FROM OUR REDUX TOOL KIT API QUERY */]
+        {/** We Create New Project we're passing in the project list that gets sent to the backend and our backend is going to utilize
+            this values once we hit submit. */}
+        await createProject({
+            name: projectName,
+            description: description,
+            startDate: startDate,
+            endDate: endDate
+        })
+
+    };
+    
+    const isFormValid = () => {
+        return projectName && description && startDate && endDate;
+    };
+
+
   return (
     <div>
         Modal New Project
